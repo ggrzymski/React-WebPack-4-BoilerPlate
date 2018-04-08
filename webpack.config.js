0817b7a3399d69c94d26016defa7a8e70c10fabe
Greg G <ggrzymski@gmail.com>
2018-04-07 23:52:31 -0400

    "ran prettier through the code"

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,48 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-  });
+  template: "./src/index.html",
+  filename: "./index.html"
+});
 
 module.exports = {
-    module: {
-        rules: [ {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
             }
-        }, {
-             test: /\.css$/,
-             use:  [
-                 { 
-                   loader: "style-loader"
-                 }, 
-                 {
-                    loader: "css-loader",
-                    options: 
-                    {
-                        modules: true,
-                        importLoaders: 1,
-                        localIdentName: "[name]_[local]_[hash:base64]",
-                        sourceMap: true,
-                        minimize: true
-                    }
-                 }
-              ]
-        }, {
-             test: /\.html$/,
-             use: [{
-             loader: "html-loader",
-             options: { minimize: true }
-          }]
-        } ]
-    },
-    plugins: [htmlPlugin]
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
 };
